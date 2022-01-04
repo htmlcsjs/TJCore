f453d62ab479f78d2b62f6da118e20a9764b6ed9
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2022-01-04 16:29:22 +0000

    fix gradle

--- src/main/java/TJCore/TJCore.java
+++ src/main/java/TJCore/TJCore.java
@@ -3,16 +3,15 @@ package TJCore;
 import codechicken.lib.CodeChickenLib;
 import gregtech.api.GTValues;
 import net.minecraftforge.fml.common.*;
-import TJCore.client.ClientProxy;
-import TJCore.common.ServerProxy;
+import TJCore.common.CommonProxy;
 
-@Mod(modid = "TJCore", name = "TJCore", acceptedMinecraftVersions = "[1.12, 1.13)", dependencies = "required:forge@[14.23.5.2847,);" + CodeChickenLib.MOD_VERSION_DEP + GTValues.MOD_VERSION_DEP + "after:forestry;after:jei@[4.15.0,);after:crafttweaker;before:ctm")
+@Mod(modid = "tjcore", name = "TJCore", acceptedMinecraftVersions = "[1.12, 1.13)", dependencies = "required:forge@[14.23.5.2847,);" + CodeChickenLib.MOD_VERSION_DEP + GTValues.MOD_VERSION_DEP + "after:forestry;after:jei@[4.15.0,);after:crafttweaker;before:ctm")
 
 public class TJCore {
     @Mod.Instance("TJCore")
     public static TJCore instance;
 
-    @SidedProxy(modId = "TJCore", clientSide = "TJCore.client.ClientProxy", serverSide = "TJCore.common.ServerProxy")
-    public static ServerProxy proxy;
+    @SidedProxy(modId = "tjcore", clientSide = "TJCore.client.ClientProxy", serverSide = "TJCore.common.ServerProxy")
+    public static CommonProxy proxy;
 
 }
