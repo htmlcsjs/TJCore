aa3e454fd4879ad1b107b9854d943ac655d83005
matthew <matthew.a.hernandez12@gmail.com>
2022-01-04 17:14:16 -0500

    New material and add lang

--- src/main/java/TJCore/common/material/TJMaterials.java
+++ src/main/java/TJCore/common/material/TJMaterials.java
@@ -1,10 +1,29 @@
 package TJCore.common.material;
 import gregtech.api.unification.material.Material;
+import gregtech.api.unification.material.info.MaterialFlag;
+import static gregtech.api.unification.material.info.MaterialFlags.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class TJMaterials {
+/*
+    public static final List<MaterialFlag> GEN = new ArrayList<>();
+    public static final List<MaterialFlag> GENWITHROD = new ArrayList<>();
+
+
+    static {
+        GEN.add(GENERATE_PLATE);
+        GEN.add(GENERATE_DENSE);
+        GENWITHROD.addAll(GEN);
+        GENWITHROD.add(GENERATE_ROD);
+        GENWITHROD.add(GENERATE_LONG_ROD);
+        GENWITHROD.add(GENERATE_BOLT_SCREW);
+    }
+*/
 
     //Secondary
     public static Material BT6;
-
+    public static Material Birmabright;
 }
--- src/main/java/TJCore/common/material/materials/TJSecondDegreeMaterials.java
+++ src/main/java/TJCore/common/material/materials/TJSecondDegreeMaterials.java
@@ -2,22 +2,31 @@ package TJCore.common.material.materials;
 
 import gregtech.api.unification.material.Material;
 import gregtech.api.unification.material.info.MaterialIconSet;
+
 import static TJCore.common.material.TJMaterials.*;
 
 
 import static gregtech.api.unification.material.Materials.*;
-import static gregtech.api.unification.material.info.MaterialFlags.GENERATE_PLATE;
+import static gregtech.api.unification.material.info.MaterialFlags.*;
 
 public class TJSecondDegreeMaterials {
     public static void register(){
-
-        BT6 = new  Material.Builder(32001, "bt6")
-                .ingot()
+        BT6 = new Material.Builder(25000, "bt6")
+                .ingot().fluid(Material.FluidType.FLUID)
                 .colorAverage().iconSet(MaterialIconSet.SHINY)
-                .flags(GENERATE_PLATE)
+                .flags(EXT2_METAL)
                 .components(Iron,3, Carbon,1, Vanadium, 5, Titanium, 40, Aluminium, 6)
                 .blastTemp(3400)
                 .build();
+
+        Birmabright = new Material.Builder(25001, "birmabright")
+                .ingot().fluid(Material.FluidType.FLUID)
+                .color(1755371).iconSet(MaterialIconSet.DULL)
+                .flags(EXT2_METAL)
+                .components(Aluminium, 5, Magnesium, 1, Manganese, 1)
+                .build();
     }
+
+
 
 }
